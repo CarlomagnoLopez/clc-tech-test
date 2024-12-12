fb7cb21f3704bb3f4b093d4cf972cfa43e3a66b1
Carlomagno <carlolc@Carlomagnos-MacBook-Pro-2.local>
2024-12-12 03:59:33 -0600

    main: adding game logic and first css aproach

--- src/components/MatchGame.tsx
+++ src/components/MatchGame.tsx
@@ -9,77 +9,110 @@ type ChildProps = {
 
 const MatchGame: FC<ChildProps> = ({ data }) => {
     const [itemsData, setItemsData] = useState(data)
-    const [itemsCountry, setItemsCountry]: any = useState([])
-    const [itemsCity, setItemsCity]: any = useState([])
+    const [targetValue, setTargetValue]: any = useState('')
+    const [currentValue, setCurrentValue]: any = useState('')
+    const [errors, setErrors]: any = useState(0)
+    const [success, setSuccess]: any = useState(0)
+    useEffect(() => {
+        if (errors !== 0) {
+            // console.log("errores")
+            console.log(errors)
+        }
+        if (success !== 0) {
+            // console.log("completados")
+            console.log(success)
+        }
+    }, [errors, success])
 
     useEffect(() => {
-        createCountryVsCity()
-    }, []);
+
+        if (targetValue !== '' && targetValue.classList.value.indexOf('active') === -1) {
+            console.log('adding style')
+            targetValue.classList.add('active');
+        }
+
+        if (currentValue !== '' && currentValue.classList.value.indexOf('active') === -1) {
+            currentValue.classList.add('active');
+            validatePair() ? setSuccess(success + 1) : setErrors(errors + 1);
+            setTimeout(() => {
+                targetValue.classList.remove('active');
+                currentValue.classList.remove('active');
+                setTargetValue('')
+                setCurrentValue('')
+            }, 1000);
+
+        }
+
+    }, [targetValue, currentValue]);
+
+    const validatePair = () => targetValue.id === currentValue.textContent;
+
+
+
+
+    // }
+
 
     const getRandomInt = (max: number) => {
 
         return Math.floor(Math.random() * max);
     }
 
-    const createCountryVsCity = () => {
-        if (itemsCountry.length === 0) {
-
-            for (const [key, value] of Object.entries(itemsData)) {
-                itemsCountry.push(key)
-                itemsCity.push(value)
-            }
-            // makeRandomOrder(itemsCountry.length)
-        }
-    }
-
-    // const makeRandomOrder = (length: any) => {
-
-
-    // }
-
-
-
-
-    // console.log(data)
     return (
+
         <div className="containerGame">
-            {itemsCountry.length > 0 && itemsCountry.map(
-                (item: any, index: number) => {
+            <div className="containerRecords">
+                <div>
+                    Completadas: {success}
+                </div>
+                <div>
+                    Erroneos: {errors}
+                </div>
+            </div>
+            <div className="containerItems">
+                {Object.entries(itemsData).map((item: any, index: number) => {
                     return (
-                        <span id={itemsCity[index]} className="country initialClass">
-                            {item}
-                        </span>)
+                        <span
+                            className={`initialClass country ${item[0]}`}
+                            onClick={(e) => {
+                                if (targetValue === '') {
+                                    setTargetValue(e.target)
+                                } else {
+                                    setCurrentValue(e.target)
+                                }
+                            }
+                            }
+                            key={`${item[0]}-${index}`}
+                            id={item[1]} >
+                            {item[0]}
+                        </span>
+                    )
                 })}
-
-            {itemsCity.length > 0 && itemsCity.map(
-                (item: any, index: number) => {
+                {Object.entries(itemsData).map((item: any, index: number) => {
                     return (
-                        <span id={itemsCountry[index]} className="city initialClass">
-                            {item}
-                        </span>)
+                        <span
+                            className={`initialClass city ${item[1]}`}
+                            onClick={(e) => {
+                                if (targetValue === '') {
+                                    setTargetValue(e.target)
+                                } else {
+                                    setCurrentValue(e.target)
+                                }
+                            }
+                            }
+                            key={`${item[1]}-${index}`}
+                            id={item[0]} >
+                            {item[1]}
+                        </span>
+                    )
                 })}
+            </div>
 
-            {/* <span className="country initialClass">
-                Country
-            </span>
-            <span className="capital initialClass">
-                Down Town
-            </span>
-            <span className="country initialClass">
-                Country
-            </span>
-            <span className="capital initialClass">
-                Down Town
-            </span>
-            <span className="country initialClass">
-                Country
-            </span>
-            <span className="capital initialClass">
-                Down Town
-            </span> */}
+
         </div>
     )
 };
 
 
 export default MatchGame;
+
